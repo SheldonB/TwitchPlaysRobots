c025e68bd234bac06b06377f7731ab8287df2072
Sheldon Burks <sheldon.burks@gmail.com>
2016-10-18 13:48:42 -0400

    Added regex checking for messages

--- app.py
+++ app.py
@@ -10,14 +10,19 @@ commands = (
         'TURN'
         )
 
+def check_message(msg):
+    if re.match(r':[0-9A-Za-z]*![0-9A-Za-z]*@[0-9A-Za-z]*.tmi.twitch.tv PRIVMSG #[0-9A-Za-z]* :[!*0-9A-Za-z_]*', msg):
+        return True
+
 def process_message(msg):
-    parsed_msg = parse_message(msg)
-    return parsed_msg
+    if check_message(msg):
+        parsed_msg = parse_message(msg)
+        return parsed_msg
 
 def parse_message(msg):
     return {
             'username': re.findall(r'^:([a-zA-Z0-9_]+)\!', msg)[0],
-            # 'message':
+            'message': re.findall(r'PRIVMSG #[0-9A-Za-z]* :([!*0-9A-Za-z_\- ]*)', msg)[0]
     }
 
 class ApplicationServer(object):
@@ -26,6 +31,7 @@ class ApplicationServer(object):
 
     def run(self):
         while True:
+            # msg = self.irc.recieve_message()
             msg = process_message(self.irc.recieve_message())
             print(msg)
 
