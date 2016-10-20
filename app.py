1a30725dbbf9b50ca79843d463603db399a3ce13
Sheldon Burks <sheldon.burks@gmail.com>
2016-10-20 13:41:02 -0400

    Added logging for when commands are entered

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-import re
-import json
-import logging
-import argparse
+import re, json, argparse, logging
 
 from lib import irc, util
-
-commands = (
-        'FORWARD',
-        'BACKWARD',
-        'TURN'
-        )
 
 class ApplicationServer(object):
     def __init__(self, config):
@@ -18,19 +9,23 @@ class ApplicationServer(object):
 
     def run(self):
         while True:
+
             msg = util.process_message(self.irc.recieve_message())
-            if msg is not None:
-                self.logger.debug('Message recieved - %s - %s' % (msg['username'], msg['message']))
+            if msg:
+                self.logger.debug('Message recieved from %s: %s' % (msg['username'], msg['message']))
+
+            if msg and 'command' in msg:
+                self.logger.info('Command recieved from %s: %s %s' % (msg['username'], msg['command'], msg['argument']))
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--config', help='Pass in a config file. That specifices sever, user, pass, and channel')
-    arg_parser.add_argument('--debug', action='store_true', help='Turn on debugging to recieve logging info') 
+    arg_parser.add_argument('--debug', action='store_true', help='Turn on debugging to recieve logging info')
     args = arg_parser.parse_args()
 
     with open(args.config) as config_file:
         config = json.load(config_file)
-    
+
     logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s')
     logger = logging.getLogger()
 
