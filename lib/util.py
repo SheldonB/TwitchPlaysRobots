6ea8f8cdf27c7a02eaf6dfbcafe85dbbe9c644af
Sheldon Burks <sheldon.burks@gmail.com>
2016-10-20 11:19:01 -0400

    Added proper command parsing. This will probbaly be refactored later once I figure out a better way to do it and the project comes together.

--- lib/util.py
+++ lib/util.py
@@ -10,7 +10,35 @@ def _check_message(msg):
         return True
 
 def _parse_message(msg):
+    parsed_msg = re.findall(r'PRIVMSG #[0-9A-Za-z]* :([!*0-9A-Za-z_\- ]*)', msg)[0]
+
+    if _check_command(parsed_msg):
+        command = _parse_command(parsed_msg)
+        argument = _parse_argument(command)
+
+        return {
+                'username': re.findall(r'^:([a-za-z0-9_]+)\!', msg)[0],
+                'message': parsed_msg,
+                'command': command,
+                'arguement': argument
+        }
+
     return {
-            'username': re.findall(r'^:([a-zA-Z0-9_]+)\!', msg)[0],
-            'message': re.findall(r'PRIVMSG #[0-9A-Za-z]* :([!*0-9A-Za-z_\- ]*)', msg)[0]
+            'username': re.findall(r'^:([a-za-z0-9_]+)\!', msg)[0],
+            'message': parsed_msg,
     }
+
+def _check_command(msg):
+    if re.match(r'![a-zA-Z]* ?[0-9]*', msg):
+        return True
+
+def _parse_command(msg):
+    return re.findall(r'!([A-Za-z]*)', msg)[0]
+
+def _parse_argument(command):
+    argument = re.findall(r'[A-Za-z]* ([0-9]*)', command)
+    if not argument:
+        argument = 1
+    else:
+        argument = argument[0]
+    return argument
